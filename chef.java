46948abbbc08154fd659ec4a1e1ce75c2cc693f8
lilpump1 <31898415+lilpump1@users.noreply.github.com>
2017-09-12 13:19:24 -0400

    More text rules

--- chef.java
+++ chef.java
@@ -13,8 +13,7 @@ public class chef {
 	 
 	 while(count < len){
 		 letter = word.charAt(count);
-	 
-		  if (count == 0) { //Beginning-of-word rules
+		  if (count == 0) { 
               if (letter == 'e') {
                   word.replaceAll("i", word);
                   count++;
@@ -33,6 +32,127 @@ public class chef {
                   continue;
               }
 	 }
+	else {  //End of Beginning-of-word rules, Start of In-Word rules
+                if (letter == 'e') {
+                    if (!isLast && word.charAt(count + 1) == 'w') {
+                        buff.append("oo");
+                        count += 2;
+                        continue;
+                    } else if (isLast) {
+                        buff.append("e-a");
+                        count++;
+                        continue;
+                    }
+                } else if (letter == 'f') {
+                    buff.append("ff");
+                    count++;
+                    continue;
+                } else if (letter == 'i') {
+                    if (!isLast && word.charAt(count + 1) == 'r') {
+                        buff.append("ur");
+                        count += 2;
+                        continue;
+                    } else if (!i_seen) {
+                        buff.append("ee");
+                        count++;
+                        i_seen = true;
+                        continue;
+                    }
+                } else if (letter == 'o') {
+                    if (!isLast && word.charAt(count + 1) == 'w') {
+                        buff.append("oo");
+                        count += 2;
+                        continue;
+                    } else {
+                        buff.append("u");
+                        count++;
+                        continue;
+                    }
+                } else if (count <= len - 4 && letter == 't' && word.charAt(count + 1) == 'i'
+                        && word.charAt(count + 2) == 'o' && word.charAt(count + 3) == 'n') {
+                    buff.append("shun");
+                    count += 4;
+                    continue;
+                } else if (letter == 'u') {
+                    buff.append("oo");
+                    count++;
+                    continue;
+                } else if (letter == 'U') {
+                    buff.append("Oo");
+                    count++;
+                    continue;
+                }
+            }  //end if In-word rules
+            //End if Word-placement rules, Start of Anywhere rules
+            if (letter == 'A') {
+                if (!isLast && word.charAt(count + 1) == 'n') {
+                    buff.append("Un");
+                    count = count + 2;
+                    continue;
+                } else if (!isLast && word.charAt(count + 1) == 'u') {
+                    buff.append("Oo");
+                    count = count + 2;
+                    continue;
+                } else if (!isLast) {
+                    buff.append("E");
+                    count++;
+                    continue;
+                }
+            } else if (letter == 'a') {
+                if (!isLast && word.charAt(count + 1) == 'n') {
+                    buff.append("un");
+                    count = count + 2;
+                    continue;
+                } else if (!isLast && word.charAt(count + 1) == 'u') {
+                    buff.append("oo");
+                    count = count + 2;
+                    continue;
+                } else if (!isLast) {
+                    buff.append("e");
+                    count++;
+                    continue;
+                }
+            } else if (letter == 'e') {
+                if (!isLast && word.charAt(count + 1) == 'n'
+                        && count == len - 2) {
+                    buff.append("ee");
+                    count += 2;
+                    continue;
+                } else if (count > 0) {
+                }
+            } else if (letter == 't') {
+                if (count == len - 2 && word.charAt(count + 1) == 'h') {
+                    buff.append("t");
+                    count += 2;
+                    continue;
+                } else if (count <= len - 3 && word.charAt(count + 1) == 'h'
+                        && word.charAt(count + 2) == 'e') {
+                    buff.append("zee");
+                    count += 3;
+                    continue;
+                }
+            } else if (letter == 'T' && count <= len - 3 && word.charAt(count + 1) == 'h'
+                    && word.charAt(count + 2) == 'e') {
+                buff.append("Zee");
+                count += 3;
+                continue;
+            } else if (letter == 'v') {
+                buff.append("f");
+                count++;
+                continue;
+            } else if (letter == 'V') {
+                buff.append("F");
+                count++;
+                continue;
+            } else if (letter == 'w') {
+                buff.append("v");
+                count++;
+                continue;
+            } else if (letter == 'W') {
+                buff.append("V");
+                count++;
+                continue;
+            }	 	 		 
  }
  }
 }
