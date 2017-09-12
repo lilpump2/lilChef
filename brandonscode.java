a0a7e7176bca197378a3d90df6906ad485c626d6
lilpump1 <31898415+lilpump1@users.noreply.github.com>
2017-09-12 13:32:29 -0400

    brandonscodedoesnothing

---
+++ brandonscode.java
@@ -0,0 +1,23 @@
+package review;
+
+public class brandonscode {
+	public static void main(String [] args){
+		String word = "Caleb Baleb";
+		int len = word.length();
+		char letter;
+		
+		int counter = 0;
+		String sub = "";
+		
+		do{
+			int counter1 = counter;
+			while(counter < len){
+				letter = word.charAt(counter);
+				if(letter == ' '){
+					sub = word.substring(counter1, counter);
+					System.out.println(sub);
+				}
+			}
+		}while(sub == "");
+	}
+}
